0c3e2ad5b522be940916ee689e2e22acdc3bc696
sassberto <matt@vistaseeker.com>
2020-06-21 15:27:24 -0700

    refactored the request counter

--- application.py
+++ application.py
@@ -21,39 +21,51 @@ app.secret_key = b'_5#ydhhL"F4Qewaxec]/'
 
 mpapi_gpx = MPAPI_GPX()
 
-#avoids exceeding the API limit for MP's API
-def check_api_throttle(increment):
-	is_throttled = 0
-	API_LIMIT=500
+API_LIMIT=250
+CACHE_CLIENT_ENDPOINT = 'localhost'
+#CACHE_CLIENT_ENDPOINT = 'gpx-cache.r6bmze.cfg.use2.cache.amazonaws.com'
+CACHE_PORT = 11211
+
+#adds one to the API request count and returns the current count
+def set_api_throttle():
 	
-	client = Client(('gpx-cache.r6bmze.cfg.use2.cache.amazonaws.com', 11211))
-	
+	client = Client((CACHE_CLIENT_ENDPOINT, CACHE_PORT))	
 	result = client.get('daily_requests')
 		
 	if (result):
-		result = int(result)	
+		result = int(result) + 1	
 	else:
 		result = 1
-
-	if increment > 0:
-		result = result + increment
 			
-	client.set('daily_requests',result, expire=86400)
+	client.set('daily_requests',result, expire=10800)
 	
-	if result >=API_LIMIT:
-		is_throttled = 1
+	return result
+
+#returns true if the next request will exceed the MountainProject API limit
+def get_api_throttle():
+	is_throttled = 0
+	
+	#memecached
+	client = Client((CACHE_CLIENT_ENDPOINT, CACHE_PORT))	
+	result = client.get('daily_requests')
+			
+	if (result):
+		result = int(result)
+		print(result)
+		if (result >= API_LIMIT):
+			is_throttled = 1
 		
 	return is_throttled
 		
 @app.route('/', methods=['GET', 'POST'])
 def show_login():
 	error = None
-	is_throttled = check_api_throttle(0) 
+	is_throttled = get_api_throttle() 
 	
 	if request.method == 'POST' and is_throttled == 0:       
 		try:
 			profile = mpapi_gpx.getMP_Profile(request.form['username'])	
-			check_api_throttle(1) 
+			set_api_throttle()
 		except(http.client.InvalidURL) as error:
 			return render_template('main.html',error=error)
 			
@@ -64,14 +76,14 @@ def show_login():
 			return render_template('main.html',error='Profile not found.')	
 	else:
 		if is_throttled:
-			error = "API limit reached for a while.  Try again later."
+			error = "The MountainProject API limit has been reached. Try again later."
 			
 		return render_template('main.html', error=error)
 		
 @app.route('/downloads/<string:username>', methods=['GET', 'POST'])
 def download(username):
 	output = mpapi_gpx.getMP_GPX(username)
-	check_api_throttle(1) 
+	set_api_throttle() 
 	resp = make_response(output)
 	resp.headers['Content-Type'] = 'text/xml;charset=UTF-8'
 	resp.headers['Content-Disposition'] = 'attachment;filename=todos.gpx'
@@ -83,8 +95,7 @@ def page_not_found(error):
 
 @app.errorhandler(500)
 def catch_all(error):
-	error = "Well that didn't seem to work."
-	return render_template('main.html', error=error)
+	return render_template('main.html', error='Well that didn\'t seem to work.')
 
 @app.template_filter('formatdatetime')
 def format_datetime(value, format="%d %b %Y %I:%M %p"):
